a58135a86976cac2c5ce9fde91144842312c5bf5
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-12-07 23:36:09 +0100

    Summarise and plot ranking

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,12 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-read.csv('data/bbc_hiphop_25.csv')
+polls <- read.csv('data/bbc_hiphop_25.csv')
+polls <- mutate(polls, points = (6 - rank) * 2)
+by_song <- group_by(polls, artist, title, year)
+ranking <- summarise(by_song, points_total = sum(points), n = n())
+
+View(ranking)
+
+ggplot(data = ranking) + 
+  geom_point(mapping = aes(x = year, y = points_total), position = "jitter")
