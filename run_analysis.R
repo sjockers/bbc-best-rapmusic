eb2a1332d158a40c7df12086a561daaef945976c
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-12-09 12:33:19 +0100

    Add artist gender to data & plot it

--- run_analysis.R
+++ run_analysis.R
@@ -3,10 +3,10 @@ library(tidyverse)
 
 polls <- read.csv('data/bbc_hiphop_25.csv')
 polls <- mutate(polls, points = (6 - rank) * 2)
-by_song <- group_by(polls, artist, title, year)
+by_song <- group_by(polls, artist, title, year, artist_gender)
 ranking <- summarise(by_song, points_total = sum(points), n = n())
 
 View(ranking)
 
 ggplot(data = ranking) + 
-  geom_point(mapping = aes(x = year, y = points_total), position = "jitter")
+  geom_point(mapping = aes(x = year, y = points_total, color = artist_gender), position = "jitter")
