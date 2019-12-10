241720fcd4b3964d37f22cd6c2ec71f7c6cb6296
Simon Jockers <449739+sjockers@users.noreply.github.com>
2019-12-10 11:31:26 +0100

    Sort ranking according to algorithm described in original BBC article
    http://www.bbc.com/culture/story/20191007-the-greatest-hip-hop-songs-of-all-time-who-voted

--- run_analysis.R
+++ run_analysis.R
@@ -2,11 +2,29 @@ install.packages("tidyverse")
 library(tidyverse)
 
 polls <- read.csv('data/bbc_hiphop_25.csv')
-polls <- mutate(polls, points = (6 - rank) * 2)
-by_song <- group_by(polls, artist, title, year, artist_gender)
-ranking <- summarise(by_song, points_total = sum(points), n = n())
+
+ranking <- polls %>% 
+  group_by(title, artist, year, artist_gender) %>% 
+  summarise(
+    points = sum(points = (6 - rank) * 2), 
+    n = n(), 
+    n1 = sum(rank == 1),
+    n2 = sum(rank == 2),
+    n3 = sum(rank == 3),
+    n4 = sum(rank == 4),
+    n5 = sum(rank == 5)
+  ) %>% 
+  arrange(
+    desc(points), 
+    desc(n),
+    desc(n1), 
+    desc(n2), 
+    desc(n3), 
+    desc(n4), 
+    desc(n5)
+  )
 
 View(ranking)
 
-ggplot(data = ranking) + 
-  geom_point(mapping = aes(x = year, y = points_total, color = artist_gender), position = "jitter")
+ggplot(data = ranking) +
+  geom_point(mapping = aes(x = year, y = points, color = artist_gender), position = "jitter")
